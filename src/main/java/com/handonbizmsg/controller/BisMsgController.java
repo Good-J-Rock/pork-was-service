ebc1a2cb3e161ff47c2a98bd69786f1a15b290c0
unarock <unafront.goodj@gmail.com>
2025-10-27 09:30:28 +0900

    pom, yml, controller

--- src/main/java/com/handonbizmsg/controller/BisMsgController.java
+++ src/main/java/com/handonbizmsg/controller/BisMsgController.java
@@ -1,11 +1,12 @@
 package com.handonbizmsg.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BisMsgController {
-    @RequestMapping(value = "")
+    @GetMapping(value = "/bizmsg")
     public String index() {
         return "Hello World";
     }
